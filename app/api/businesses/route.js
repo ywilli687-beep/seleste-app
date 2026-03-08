06b4ddd2801b9e41e2e747987a900b3886fe6366
Yannick Williams <yannickwilliams@Yannicks-MacBook-Air.local>
2026-03-08 19:08:37 -0400

    Fix API route static rendering Vercel issue

--- app/api/businesses/route.js
+++ app/api/businesses/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../lib/prisma';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     try {
         const user = await prisma.user.findUnique({ where: { email: 'demo@seleste.com' } });
